2ebea64e4c328d7ee403a181b7f03910afb7fddf
LuzButter <simple135139@gmail.com>
2022-08-03 22:45:25 +0800

    movement and basic tilemap

---
+++ Meow_project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MoveSpeed;
+    public Rigidbody2D rb;
+    public Vector2 moveDir;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInpute();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInpute()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+
+        if(moveX == 0f && moveY == 0f)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        else
+        {
+            animator.SetBool("isMoving", true);
+        }
+        if(moveX > 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(moveX < 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x*MoveSpeed*Time.fixedDeltaTime, moveDir.y*MoveSpeed*Time.fixedDeltaTime);
+    }
+}
+
+   
